ee3e474d8bee4eb2c6475e2f4bce9afb757bd2a5
Beatriz Correia <beatriz.correia.pt@gmail.com>
2020-04-12 20:28:13 +0100

    add object Document.py + add doc indexation

---
+++ Document.py
@@ -0,0 +1,19 @@
+class Document:
+
+	def __init__(self, docID, docTitle, docContent, docAll):
+		self.docID = docID
+		self.docTitle = docTitle
+		self.docContent = docContent
+		self.docAll = docAll
+
+	def get_docID(self):
+		return self.docID
+
+	def get_docTitle(self):
+		return self.docTitle
+
+	def get_docContent(self):
+		return self.docContent
+
+	def get_docAll(self):
+		return self.docAll
---
+++ retrieval_system.py
@@ -0,0 +1,81 @@
+import os
+import sys
+import shutil
+from whoosh.index import create_in
+from whoosh.index import open_dir
+from whoosh.fields import Schema, TEXT, NUMERIC
+from Document import Document
+
+fp_allDocs = "/home/beatriz/Documents/TIS/health_informatics_project2/med/MED.ALL"
+
+def parseAllDocs():
+	allDocs = []
+	with open(fp_allDocs) as fp:
+		line = fp.readline()
+		flagInsideDoc = 0
+		while line:
+			if (flagInsideDoc == 0) and (".I" in line.strip()):
+				flagInsideDoc = 1
+				docID = ''.join(filter(str.isdigit, line))
+				docAll = ''
+			elif (line.strip() == ".W"):
+				pass
+			elif (flagInsideDoc == 1) and (".I" not in line.strip()):
+				docAll += line.strip()
+				docAll += ' '
+			elif (flagInsideDoc == 1) and (".I" in line.strip()):
+				flagInsideDoc = 0
+				docTitle = ''
+				for char in docAll:
+					if (char != '.'):
+						docTitle += char
+					elif (char == '.'):
+						break
+				docTitle = docTitle + '. '
+				docContent = docAll.replace(docTitle, "")
+				document = Document(int(docID), docTitle, docContent, docAll)
+				allDocs.append(document)
+				continue
+			line = fp.readline()
+		docTitle = ''
+		for char in docAll:
+			if (char != '.'):
+				docTitle += char
+			elif (char == '.'):
+				break
+		docTitle = docTitle + '. '
+		docContent = docAll.replace(docTitle, "")
+		document = Document(int(docID), docTitle, docContent, docAll)
+		allDocs.append(document)
+	fp.close()
+	return allDocs
+
+def indexMedlineCollection(allDocs):
+	schema = Schema(docID = NUMERIC(unique = True, stored = True), docTitle = TEXT(stored = True), docContent = TEXT(stored = True), docAll = TEXT(stored = True))
+	if not os.path.exists("indexdir"):
+		os.mkdir("indexdir")
+	index = create_in("indexdir", schema)
+	writer = index.writer()
+	for doc in allDocs:
+		writer.add_document(docID = doc.get_docID(), docTitle = doc.get_docTitle(), docContent = doc.get_docContent(), docAll = doc.get_docAll())
+	writer.commit()
+
+def checkDocumentsIndexed():
+	ix = open_dir('indexdir')
+	allDocs = ix.searcher().documents()
+	for doc in allDocs:
+		print(doc)
+
+if __name__ == '__main__':
+
+	# PARSES DOCUMENTS FROM MED.ALL INTO OBJECTS OF TYPE DOCUMENT
+	allDocs = parseAllDocs()
+
+	# INDEXES THE DOCUMENTS WITH RESPECT TO THE SCHEMA DEFINED
+	indexMedlineCollection(allDocs)
+
+	# PRINTS THE INDEXATION GENERATED
+	checkDocumentsIndexed()
+
+	# REMOVES THE DIRECTORY CREATED FOR THE INDEXING
+	shutil.rmtree("indexdir")
